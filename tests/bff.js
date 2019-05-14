706c2021cfa124e71d5eeacd080f9d40e73dc29d
Ian Walter <public@iankwalter.com>
2019-05-14 09:28:27 -0400

    More test refactoring

--- tests/bff.js
+++ tests/bff.js
@@ -1,7 +1,8 @@
 const { test, run } = require('..')
 
 test('bff', async ({ expect }) => {
-  const result = await run({ timeout: 5000 })
+  const config = { timeout: 5000, plugins: ['tests/helpers/plugin.js'] }
+  const result = await run(config)
   expect(result.passed).toBe(15)
   expect(result.failed).toBe(8)
   expect(result.skipped).toBe(2)
