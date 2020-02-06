fae81c49a476013dcd9b1713d2e659c7f2e22df1
Ian Walter <public@iankwalter.com>
2020-02-06 15:57:04 -0500

    Updating snapshots

--- tests/fail.tests.js
+++ tests/fail.tests.js
@@ -4,8 +4,7 @@ const { html } = require('common-tags')
 test('no assertions', () => true)
 
 test('parseInt with the wrong base', ({ expect }) => {
-  const two = '2'
-  expect(parseInt(two, 2)).toBe(2)
+  expect(parseInt('2', 2)).toBe(2)
 })
 
 test('manual fail', ({ fail }) => fail())
