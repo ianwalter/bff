5b54b5c48919a8a46cb7405e6ada22a9e09a0248
Ian Walter <pub@ianwalter.dev>
2020-05-06 23:53:57 -0400

    Adding #205

--- tests/bff.js
+++ tests/bff.js
@@ -12,6 +12,8 @@ const config = {
 const toName = ({ name }) => name
 const execaOpts = { reject: false }
 
+// This covers running all of the tests via the run function and verifying that
+// the results match their snapshots and the status counts are correct.
 test('bff', async ({ expect }) => {
   const result = await run(config)
   expect(result.filesRegistered).toBe(5)
@@ -27,11 +29,13 @@ test('bff', async ({ expect }) => {
   expect(result.skipped.map(toName).sort()).toMatchSnapshot()
 })
 
+// This tests the fail-fast option via CLI.
 test('bff --fail-fast', async ({ expect }) => {
   const { stdout } = await execa('./cli.js', ['--fail-fast'], execaOpts)
   expect(stdout).toContain(FailFastError.message)
 })
 
+// This tests the tag option via the run function.
 test('bff --tag qa', async ({ expect }) => {
   const result = await run({ ...config, tag: 'qa' })
   expect(result.testsRegistered).toBe(2)
@@ -40,6 +44,7 @@ test('bff --tag qa', async ({ expect }) => {
   expect(result.failed.length).toBe(1)
 })
 
+// This tests the --every tag option via the run function.
 test('bff --tag dev --tag qa --match every', async ({ expect }) => {
   const result = await run({ ...config, tag: ['dev', 'qa'], match: 'every' })
   expect(result.testsRegistered).toBe(1)
@@ -48,15 +53,27 @@ test('bff --tag dev --tag qa --match every', async ({ expect }) => {
   expect(result.failed.length).toBe(1)
 })
 
+// This tests that uncaught exceptions in test files outside of tests functions
+// are caught and fail the test suite via the CLI.
 test('uncaught exception in test file', async ({ expect }) => {
   const { stdout } = await execa('./cli.js', ['tests/uncaught.js'], execaOpts)
   expect(stdout).toContain("Cannot find module 'thing-that-doesnt-exist'")
 })
 
-test('junit', async ({ expect }) => {
+// This tests that the test results are outputted to a JUnit file properly via
+// the CLI.
+test('bff --junit', async ({ expect }) => {
   await execa('./cli.js', ['--timeout', config.timeout, '--junit'], execaOpts)
   const junit = await fs.readFile(path.resolve('junit.xml'), 'utf8')
   // TODO: Fix toMatchSnapshotLines() to properly unescape string.
   // expect(junit).toMatchSnapshotLines()
   expect(junit).toBeDefined()
+})
+
+// This tests that the --runs option runs the test suite the specified amount of
+// times via the CLI.
+test('bff --runs 2', async t => {
+  const args = ['--timeout', config.timeout, '--runs', '2']
+  const { stdout } = await execa('./cli.js', args, execaOpts)
+  t.expect(stdout.split('10 failed').length).toBe(3)
 })
--- tests/helpers/plugin.js
+++ tests/helpers/plugin.js
@@ -1,9 +1,6 @@
-const { print } = require('@ianwalter/print')
-
 module.exports = {
   before (context) {
     context.beforeMessage = 'before was here'
-    print.log('before executed')
   },
   registration (file, context) {
     context.augmentTests = tests => tests.reduce(
@@ -21,20 +18,5 @@ module.exports = {
   },
   beforeEach (file, context) {
     context.beforeEachMessage = 'beforeEach was here'
-    if (context.testContext.name === 'beforeEach') {
-      print.log('beforeEach executed', context.beforeMessage)
-    }
-  },
-  afterEach (file, context) {
-    if (context.testContext.name === 'afterEach') {
-      print.log(
-        'afterEach executed',
-        context.beforeMessage,
-        context.beforeEachMessage
-      )
-    }
-  },
-  after (context) {
-    print.log('after executed', context.beforeMessage)
   }
 }
