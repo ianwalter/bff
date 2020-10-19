836f8f93e130fe4d24db0d8a9af82d3d5c568944
Ian Walter <pub@ianwalter.dev>
2020-10-19 15:33:11 -0400

    Fixing cli

--- packages/bff-webdriver/cli.js
+++ packages/bff-webdriver/cli.js
@@ -4,27 +4,27 @@ const cli = require('@ianwalter/cli')
 const { createLogger } = require('@generates/logger')
 const { webdriverVersion } = require('.')
 
-const logger = createLogger({ level: 'info', namespace: 'bff-webdriver' })
-
 async function run () {
   const config = cli({
     name: 'bff',
     options: {
       log: {
         alias: 'l',
-        description: "Specifies bff-webdriver's print (logging) configuration",
+        description: "Specifies bff-webdriver's logging configuration",
         default: { level: 'info' }
       }
     }
   })
   const { _: [command] } = config
 
+  const logger = createLogger({ ...config.log, namespace: 'bff-webdriver.cli' })
+
   try {
     if (command === 'setup') {
       const selenium = require('selenium-standalone')
       const { version = webdriverVersion, drivers } = config.webdriver || {}
       await new Promise((resolve, reject) => {
-        selenium.install({ logger: logger.log, version, drivers }, err => {
+        selenium.install({ version, drivers }, err => {
           if (err) {
             reject(err)
           } else {
