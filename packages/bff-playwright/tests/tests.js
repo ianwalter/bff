e86b897f5da028e3408f8770a1c9deb567e32246
Ian Walter <pub@ianwalter.dev>
2020-12-28 23:39:04 -0500

    eslit

--- packages/bff-playwright/tests/tests.js
+++ packages/bff-playwright/tests/tests.js
@@ -3,7 +3,7 @@ import { test } from '@ianwalter/bff'
 test('My personal site', async t => {
   const { page } = await t.chromium({ args: ['--no-sandbox'] })
   await page.goto('https://ianwalter.dev')
-  t.expect(await page.innerText('body')).toContain('Ian Walter, Dev')
+  t.expect(await page.innerText('body')).toContain('Ian Walter')
 })
 
 test.skip('My GitHub profile', async t => {
