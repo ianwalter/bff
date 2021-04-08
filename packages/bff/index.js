83e4cb97135bd8b922d59f8097964e5c3da53145
Ian Walter <pub@ianwalter.dev>
2021-04-08 14:44:27 -0400

    Add after hook to sigint handler

--- packages/bff/index.js
+++ packages/bff/index.js
@@ -97,7 +97,7 @@ export async function run (config) {
   })
 
   // Handle <ctrl>c / SIGINT events.
-  rl.on('SIGINT', function onSigint () {
+  rl.on('SIGINT', async function onSigint () {
     // Inform the user that the event has been received.
     logger.write('\n')
     if (context.receivedSigint) {
@@ -118,14 +118,15 @@ export async function run (config) {
     // Terminate the registration workers immediately.
     registrationPool.terminate(true)
 
+    //
+    await pSeries(context.plugins?.map(toHookRun('after', context)))
+
     // Forward the SIGINT to the test workers via the seppuku task.
     for (const worker of runPool.workers) worker.exec('seppuku')
   })
 
   // Sequentially run any before hooks specified by plugins.
-  if (context.plugins && context.plugins.length) {
-    await pSeries(context.plugins.map(toHookRun('before', context)))
-  }
+  await pSeries(context.plugins?.map(toHookRun('before', context)))
 
   try {
     // For each test file found, pass the filename to a registration pool
@@ -261,9 +262,7 @@ export async function run (config) {
   }
 
   // Sequentially run any after hooks specified by plugins.
-  if (context.plugins && context.plugins.length) {
-    await pSeries(context.plugins.map(toHookRun('after', context)))
-  }
+  await pSeries(context.plugins?.map(toHookRun('after', context)))
 
   // Terminate the run pool now that all tests have been run.
   runPool.terminate().then(() => logger.debug('Run pool terminated'))
